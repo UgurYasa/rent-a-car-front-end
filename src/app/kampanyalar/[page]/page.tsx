05be0e94f6385ba8665b487aad3fbe98d7746d06
UgurYasa <u.yasa2001@gmail.com>
2024-05-08 09:06:49 +0300

    feat: Add Select component for campaign titles in kampanyalar page

--- src/app/kampanyalar/[page]/page.tsx
+++ src/app/kampanyalar/[page]/page.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/Header";
 import Kvkk from "@/components/Kvkk";
 import NewCard from "@/components/NewCard";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   QualiztoCampaigns,
   QualiztoCorporateCampaigns,
@@ -14,6 +14,7 @@ import {
 } from "@/constants/campaigns";
 import Breadcumbs from "@/components/Breadcumbs";
 import { useRouter } from "next/navigation";
+import Select from "react-select";
 
 export default function Kampanyalar({ params }: any) {
   var router = useRouter();
@@ -27,15 +28,46 @@ export default function Kampanyalar({ params }: any) {
       ? campaignsTitles[1]
       : path === "diger-avantajlar"
       ? campaignsTitles[2]
-      :  campaignsTitles[3];
-  const content= path === "qualizto-kampanyalari"
-  ? QualiztoCampaigns
-  : path === "qualizto-kurumsal-kampanyalari"
-  ? QualiztoCorporateCampaigns
-  : path === "diger-avantajlar"
-  ? OtherAdvantages
-  : QualiztoCooperationAgreements;
+      : campaignsTitles[3];
+  const content =
+    path === "qualizto-kampanyalari"
+      ? QualiztoCampaigns
+      : path === "qualizto-kurumsal-kampanyalari"
+      ? QualiztoCorporateCampaigns
+      : path === "diger-avantajlar"
+      ? OtherAdvantages
+      : QualiztoCooperationAgreements;
   routeList.push(route);
+  const [selectedTitle, setSelectedTitle] = useState(
+    {
+      value: route,
+      label: route,
+    }
+  );
+  const options = campaignsTitles.map((title) => ({
+    value: title,
+    label: title,
+  }));
+  
+  useEffect(() => {
+    var endpoint = "qualizto-kampanyalari";
+    if (selectedTitle && selectedTitle.value) {
+      
+       endpoint = selectedTitle.value
+      .toLowerCase()
+      .replaceAll(" ", "-")
+      .replaceAll("ş", "s")
+      .replaceAll("ı", "i")
+      .replaceAll("ğ", "g")
+      .replaceAll("ü", "u")
+      .replaceAll("ö", "o")
+      .replaceAll("ç", "c");
+    } 
+     
+
+      router.push(`/kampanyalar/${endpoint}`);
+   
+  }, [selectedTitle]);
   
   return (
     <div>
@@ -43,46 +75,52 @@ export default function Kampanyalar({ params }: any) {
       <div className="flex items-center justify-center h-28 bg-gradient-to-b from-orange-600 to-orange-400 xl:text-3xl lg:text-2xl text-lg font-black text-white">
         {route}
       </div>
-      <table className=" hidden md:block border-collapse  bg-[#00285f] container">
-          <thead>
-            <tr>
-              {campaignsTitles.map((title, index) => (
-                <th
-                  key={index}
-                  className={`py-2 px-10 text-white font-normal  ${
-                    title
-                      .toLowerCase()
-                      .replaceAll(" ", "-")
-                      .replaceAll("ş", "s")
-                      .replaceAll("ı", "i")
-                      .replaceAll("ğ", "g")
-                      .replaceAll("ü", "u")
-                      .replaceAll("ö", "o")
-                      .replaceAll("ç", "c") === path
-                      ? "bg-orange-600"
-                      : "bg-[#00285f]"
-                  } border-r hover:bg-orange-500 cursor-pointer`}
-                  onClick={() => {
-                    const endpoint = title
-                      .toLowerCase()
-                      .replaceAll(" ", "-")
-                      .replaceAll("ş", "s")
-                      .replaceAll("ı", "i")
-                      .replaceAll("ğ", "g")
-                      .replaceAll("ü", "u")
-                      .replaceAll("ö", "o")
-                      .replaceAll("ç", "c");
+      <div className="w-full flex justify-center md:mb-0 mb-20 bg-[#00285f]">
+        <div className="lg:hidden bg-red-500 w-full">
+          <Select
+            options={options}
+            value={selectedTitle}
+            onChange={(selectedOption: any) => {
+              if (selectedOption && selectedOption.value) {
+                setSelectedTitle(selectedOption);
+              } else {
+                
+              }
+            }}
+            placeholder={options[0].label}
+            className="w-full"
+          />
+        </div>
+      </div>
+      <table className=" hidden lg:block border-collapse  bg-[#00285f] container">
+        <thead>
+          <tr>
+            {campaignsTitles.map((title, index) => (
+              <th
+                key={index}
+                className={`py-2 px-10 text-white font-normal  ${
+                  title === route ? "bg-orange-600" : "bg-[#00285f]"
+                } border-r hover:bg-orange-500 cursor-pointer`}
+                onClick={() => {
+                  const endpoint = title
+                    .toLowerCase()
+                    .replaceAll(" ", "-")
+                    .replaceAll("ş", "s")
+                    .replaceAll("ı", "i")
+                    .replaceAll("ğ", "g")
+                    .replaceAll("ü", "u")
+                    .replaceAll("ö", "o")
+                    .replaceAll("ç", "c");
 
-                    router.push(`/kampanyalar/${endpoint}`);
-                  }}
-                >
-                  {title}
-                </th>
-              ))}
-            </tr>
-          </thead>
-        </table>
-     
+                  router.push(`/kampanyalar/${endpoint}`);
+                }}
+              >
+                {title}
+              </th>
+            ))}
+          </tr>
+        </thead>
+      </table>
 
       <div className="flex md:flex-row flex-col md:w-5/6 mx-auto  h-20 items-center justify-between max-md:mb-5">
         <div className="flex flex-row items-center gap-2">
@@ -93,7 +131,7 @@ export default function Kampanyalar({ params }: any) {
         </div>
         <div className="flex flex-row items-center gap-2 text-white">
           <button className="px-[14px] py-[6px] bg-orange-400">
-          {/* bg-[#001a3d] hover:bg-orange-400 */}
+            {/* bg-[#001a3d] hover:bg-orange-400 */}
             TÜMÜ(5)
           </button>
           <button className="px-[14px] py-[6px] bg-[#001a3d] hover:bg-orange-400">
@@ -107,7 +145,7 @@ export default function Kampanyalar({ params }: any) {
       <div className="flex items-center justify-center w-full max-md:px-5">
         <div className="grid xl:grid-cols-3 grid-cols-2 max-sm:grid-cols-1 gap-1 md:w-5/6 w-full border-t-[1px]">
           {content.map((item: any, index: number) => (
-            <Link href={`/kampanya-detail/${path+"_"+index}`} key={index}>
+            <Link href={`/kampanya-detail/${path + "_" + index}`} key={index}>
               <NewCard
                 key={index}
                 imageUrl={item.imageUrl}
